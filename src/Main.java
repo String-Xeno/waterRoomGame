1654616b0608c21b901133a600bca755945d9d9a
String-Xeno <the1flying2gamer345@gmail.com>
2024-11-28 12:24:20 +0000

    done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,65 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("enter random number");
-        Scanner input = new Scanner(System.in);
-        int randomnumber = input.nextInt();
-        System.out.println(randomnumber);
-        System.out.println("done");
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many liters are in the room?");
+        int litersinroom = input.nextInt();
+        System.out.println("How many liters can bucket 1 take at a time?");
+        int bucket1capacity = input.nextInt();
+        System.out.println("How many liters can bucket 2 take at a time?");
+        int bucket2capacity = input.nextInt();
+
+        int litersinbucket1 = 0;
+        int litersinbucket2 = 0;
+        int spaceleftin1 = bucket1capacity-litersinbucket1;
+        int spaceleftin2 = bucket2capacity-litersinbucket2;
+
+
+        while (litersinroom > 0 || litersinbucket1 > 0 || litersinbucket2 > 0) {
+            System.out.println("The room is filled with " + litersinroom + "! \n you have to make it so the room has no water in it and clear the buckets too.");
+            System.out.println("Bucket 1 has " + litersinbucket1 + "and has room for " + spaceleftin1 + "more liters");
+            System.out.println("Bucket 2 has " + litersinbucket2 + "and has room for " + spaceleftin2 + "more liters");
+            System.out.println("\n\n Controls:\n 1.fill bucket 1 from the room");
+            System.out.println("fill bucket 2 from bucket 1");
+            System.out.println("clear bucket 2 to the garden");
+            System.out.println("\n what are you gonna do?\n");
+            char choice = input.next().charAt(0);
+            //choice 1
+            int canfill = bucket1capacity - litersinbucket1;
+            if (choice == '1') {
+                if (litersinbucket1 == 0) {
+                    litersinbucket1 = bucket1capacity; // sets the bucket to max capacity
+                    litersinroom = litersinroom - canfill; //completely fills bucket 1 with the space available
+                    System.out.println("filled bucket 1");
+                }
+                else {
+                    System.out.println("Bucket 1 is full! choose something else!");
+                }
+            }
+            //choice 2
+
+            if (choice == '2') {
+                if (litersinbucket2 < bucket2capacity) {
+                    if (spaceleftin2 <= litersinbucket1) {
+                        litersinbucket2 = litersinbucket2 + spaceleftin2; // only fills the amount of liters bucket 2 can take
+                        litersinbucket1 = litersinbucket1 - spaceleftin2; //removes the liters that u filled into bucket2
+                        spaceleftin2 = 0; // u filled bucket so no space left
+                    }
+                    if (spaceleftin2 > litersinbucket1) {
+                        litersinbucket2 = litersinbucket2 + litersinbucket1;
+                        litersinbucket1 = 0;
+                        spaceleftin2 = spaceleftin2 - litersinbucket1;
+                    }
+                }
+                System.out.println("filled bucket 2 from bucket 1!");
+
+            }
+            if (choice == '3') {
+                litersinbucket2 = 0;
+                System.out.println("cleared bucket 2 into the garden!");
+            }
+        }
     }
 }
