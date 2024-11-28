27ab339fee894bf1052361acbfb13e9731a7813c
String-Xeno <the1flying2gamer345@gmail.com>
2024-11-28 12:22:24 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("enter random number");
+        Scanner input = new Scanner(System.in);
+        int randomnumber = input.nextInt();
+        System.out.println(randomnumber);
+        System.out.println("done");
+
+    }
+}
